6b304949407328a6bed675c105fc37d553ea041a
Shukurullo <81511940+Shukurullodev@users.noreply.github.com>
2022-04-16 10:38:43 +0500

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fw9wzg1s#$osbq7kuwu@p5adchq)rx16j35dm7sd2_ud%_x(s8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['extrablok.herokuapp.com', '127.0.0.1:8000']
 MIDDLEWARE_CLASSES = ('whitenoise.middleware.WhiteNoiseMiddleware')
