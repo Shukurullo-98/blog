1178817011faa0cea8389844ef9c464b3545f36f
Shukurullo <81511940+Shukurullodev@users.noreply.github.com>
2022-04-16 11:07:12 +0500

    .

--- blog/settings.py
+++ blog/settings.py
@@ -14,6 +14,8 @@ from ntpath import join
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -25,12 +27,13 @@ SECRET_KEY = 'django-insecure-fw9wzg1s#$osbq7kuwu@p5adchq)rx16j35dm7sd2_ud%_x(s8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['extrablok.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['extrablok.herokuapp.com']
 MIDDLEWARE_CLASSES = ('whitenoise.middleware.WhiteNoiseMiddleware')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Application definition
 
 INSTALLED_APPS = [
+    'witenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
