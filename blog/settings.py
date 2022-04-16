160654409e1ddb2089853defbd88023be66b1c79
Shukurullo <81511940+Shukurullodev@users.noreply.github.com>
2022-04-16 10:33:00 +0500

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fw9wzg1s#$osbq7kuwu@p5adchq)rx16j35dm7sd2_ud%_x(s8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 MIDDLEWARE_CLASSES = ('whitenoise.middleware.WhiteNoiseMiddleware')
